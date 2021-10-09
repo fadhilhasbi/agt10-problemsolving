75cc67ec6cb91fc3914d67d13301b82fa8abddc6
MUHAMMAD FADHIL HASBI <muhammad1900016050@webmail.uad.ac.id>
2021-10-09 19:08:37 +0900

    Add Solution for Problem 2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + new Vector2(3f, 2f) * Time.deltaTime);
+    }
+}
