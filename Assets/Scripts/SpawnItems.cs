46f751f5eedd25b1591b26aef80c8d8524cf4a8d
MUHAMMAD FADHIL HASBI <muhammad1900016050@webmail.uad.ac.id>
2021-10-10 19:55:24 +0900

    Fixing Script for SpawnTimes.cs

--- Assets/Scripts/SpawnItems.cs
+++ Assets/Scripts/SpawnItems.cs
@@ -3,28 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnItems : MonoBehaviour
-{    
-    [SerializeField]
-    private float time = 2f; //Menentukan waktu spawn
-
+{
     public GameObject[] Collectibles; //Menyimpan banyak/size prefab gameobject
 
-    public Vector3 center; //Menentukan posisi center spawn
-    public Vector3 size; // Menentukan ukuran vector3 untuk posisi spawn
-
-    private void Start()
+    float randVert, randHoriz, randTotal;
+    void Start()
     {
-        StartCoroutine(SpawnCollectible()); //Menjalankan Function Coroutine untuk spawn collectibles
-    }
-
-    IEnumerator SpawnCollectible()
-    {
-        //Menetapkan posisi berdasarkan variabel yang diinisiasi
-        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), 0);
-
-        //Create spawn collectibles berdasarkan waktu yang sudah ditentukan dan posisi random yang didapatkan
-        Instantiate(Collectibles[Random.Range(0, Collectibles.Length)], pos, Quaternion.identity);
-        yield return new WaitForSeconds(time);
-        StartCoroutine(SpawnCollectible());
+        randTotal = Random.Range(4, 16);
+        for (int i = 0; i < randTotal; i++)
+        {
+            randVert = Random.Range(3.37f, -3.37f);
+            randHoriz = Random.Range(6.32f, -6.32f);
+            Instantiate(Collectibles[Random.Range(0, Collectibles.Length)], new Vector2(randHoriz, randVert), Quaternion.identity);
+        }
     }
 }
