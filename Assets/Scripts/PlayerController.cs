3a080ff6e29be6758dc4836a0ca265663d7fe4d8
MUHAMMAD FADHIL HASBI <muhammad1900016050@webmail.uad.ac.id>
2021-10-10 19:59:38 +0900

    Solution for Problem 7

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10f;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(new Vector2(0, speed));
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(new Vector2(0, -speed));
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(new Vector2(speed, 0));
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(new Vector2(-speed, 0));
+        }
+        else
+        {
+
+        }
+    }
+}
