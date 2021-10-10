83eddaafd8ad0693649ceef8c02fc0567a3aabd2
MUHAMMAD FADHIL HASBI <muhammad1900016050@webmail.uad.ac.id>
2021-10-10 17:46:07 +0900

    Add Solution for Problem 6

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    // Inisiasi komponen Rigidbody2D dengan nama variabel rb
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        //Mengambil komponen Rigidbody2D
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        //Membuat object bergerak menggunakan atribut AddForce di rigidbody2D
+        rb.AddForce(new Vector2(3, 2));
+    }
+}
---
+++ Assets/Scripts/SpawnItems.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItems : MonoBehaviour
+{    
+    [SerializeField]
+    private float time = 2f; //Menentukan waktu spawn
+
+    public GameObject[] Collectibles; //Menyimpan banyak/size prefab gameobject
+
+    public Vector3 center; //Menentukan posisi center spawn
+    public Vector3 size; // Menentukan ukuran vector3 untuk posisi spawn
+
+    private void Start()
+    {
+        StartCoroutine(SpawnCollectible()); //Menjalankan Function Coroutine untuk spawn collectibles
+    }
+
+    IEnumerator SpawnCollectible()
+    {
+        //Menetapkan posisi berdasarkan variabel yang diinisiasi
+        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), 0);
+
+        //Create spawn collectibles berdasarkan waktu yang sudah ditentukan dan posisi random yang didapatkan
+        Instantiate(Collectibles[Random.Range(0, Collectibles.Length)], pos, Quaternion.identity);
+        yield return new WaitForSeconds(time);
+        StartCoroutine(SpawnCollectible());
+    }
+}
